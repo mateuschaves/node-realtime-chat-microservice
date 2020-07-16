bd346d52f1fe9ec851b170c7d9ab871b41eed131
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-07-16 11:58:09 -0300

    enhancement resolved #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://messenger_database/node-realtime-chat-microservice',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
