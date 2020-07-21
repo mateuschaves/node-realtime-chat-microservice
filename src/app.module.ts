84e24514350cfae370323431092116be4de7f368
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-07-21 13:58:56 -0300

    remove app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConversationModule } from './conversation/conversation.module';
 import { MessagesModule } from './messages/messages.module';
-import { Conversation } from './conversation/conversation.entity';
-import { MessagesGateway } from './messages/messages.gateway';
-import { Message } from './messages/message.entity';
 
 import { typeOrmConfig } from './config/typeorm.config';
 
