e2510a4adbae1e88cd77237c3fdb689f8c71c8aa
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-08-07 15:36:12 -0300

    port type fix

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,7 +7,7 @@ const dbConfig: any = config.get('db');
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
   host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORT,
+  port: Number(process.env.RDS_PORT),
   username: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   database: process.env.RDS_DB_NAME,
